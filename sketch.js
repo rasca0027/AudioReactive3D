c633f34fdabd26e67612a8355147f622b94303a6
rasca0027 <rasca0027@gmail.com>
2017-03-03 17:56:09 -0500

    fix file format

--- sketch.js
+++ sketch.js
@@ -12,7 +12,7 @@ var spectrum, fft;
 function preload(){
   img = loadImage("data/bg2.jpg");
   sun = loadImage("data/gradient.png");
-  sound = loadSound("test.ogg");  
+  sound = loadSound("Adventura.mp3");  
 }
 
 
