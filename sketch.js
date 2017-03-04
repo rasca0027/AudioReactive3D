2915fbfd8a232f9dc902b9cbb79c25afc8dc164a
rasca0027 <rasca0027@gmail.com>
2017-03-04 12:48:34 -0500

    fix start process

--- sketch.js
+++ sketch.js
@@ -7,6 +7,7 @@ var ys = [];
 var road;
 var img, sun, sound, btn;
 var spectrum, fft;
+var initial = false;
 
 
 function preload(){
@@ -17,15 +18,19 @@ function preload(){
 
 
 function setup() {
-  
   createCanvas(800, 600, WEBGL);
-  
-  amp = new p5.Amplitude();
-  fft = new p5.FFT();
-  
   btn = createImg("data/play.png");
   btn.size(150, 150);
   btn.position(width/2-75, height/2-50);
+  
+}
+
+
+function init() {
+  
+  
+  amp = new p5.Amplitude();
+  fft = new p5.FFT();
   
   pointLight(244, 195, 100, 0, 0.5, -0.1);
   //pointLight(255, 255, 255, 0, 0.5, -0.1);
@@ -58,6 +63,10 @@ function setup() {
 function draw() {
   
   if (sound.isPlaying()) {
+    if (!initial) {
+      init();
+      initial = true;
+    }
     btn.remove();
     if (frameCount % 6 == 0) {
       var level = amp.getLevel();
@@ -66,119 +75,121 @@ function draw() {
     }
   }
   
-  spectrum = fft.analyze();
   
-  background(0);
-
-  // Draw the plain road.
+  if (initial) {
+    spectrum = fft.analyze();
+    
+    background(0);
   
-  push();
-  translate(0, 200, -50);
-  rotateX(-PI / 2);
-  texture(img);
-  plane(250, 1800);
-  rotateX(PI / 2);
-  pop();
+    // Draw the plain road.
+    
+    push();
+    translate(0, 200, -50);
+    rotateX(-PI / 2);
+    texture(img);
+    plane(250, 1800);
+    rotateX(PI / 2);
+    pop();
+    
+    
   
-  
-
-  if (frameCount % 2000 == 0) {
-    generateMoreBuilding();
-  }
-  
-  // draw sun
-  push();
-  translate(0, -600, -3500);
-  rotateY(HALF_PI);
-  texture(sun);
-  sphere(650);
-  pop();
-  
-  // something on the horizon
-  push();
-  translate(-1600, 100, -2900);
-  ambientMaterial(20, 237, 220);
-  box(1500, 10, 10);
-  for (var i = 0;i <= 60; i++) {
-    translate(50, 0, 0);
+    if (frameCount % 2000 == 0) {
+      generateMoreBuilding();
+    }
+    
+    // draw sun
+    push();
+    translate(0, -600, -3500);
+    rotateY(HALF_PI);
+    texture(sun);
+    sphere(650);
+    pop();
+    
+    // something on the horizon
+    push();
+    translate(-1600, 100, -2900);
     ambientMaterial(20, 237, 220);
-    box(xs[i], ys[i], 10);
-    push();
-    translate(0, 0, 100);
-    fill(0);
-    box(xs[i]-10, ys[i]-10, 10);
+    box(1500, 10, 10);
+    for (var i = 0;i <= 60; i++) {
+      translate(50, 0, 0);
+      ambientMaterial(20, 237, 220);
+      box(xs[i], ys[i], 10);
+      push();
+      translate(0, 0, 100);
+      fill(0);
+      box(xs[i]-10, ys[i]-10, 10);
+      pop();
+    }
     pop();
-  }
-  pop();
+    
+    
+    // camera
+    if (sound.isPlaying()) {
+      camera(0, 0, -frameCount % 1000 * 10 - 100);
+    } else {
+      camera(0, 0, 0);
+    }
+    
+    // Draw the plain road.
+    
+    translate(0, 180, 0);
+    if (frameCount % 180 == 0) {
+      road.fly();
+    }
+    road.roll();
+    
   
-  
-  // camera
-  if (sound.isPlaying()) {
-    camera(0, 0, -frameCount % 1000 * 10 - 100);
-  } else {
-    camera(0, 0, 0);
-  }
-  
-  // Draw the plain road.
-  
-  translate(0, 180, 0);
-  if (frameCount % 180 == 0) {
-    road.fly();
-  }
-  road.roll();
-  
-
-  // Draw Builidngs on left-hand side!
-  push();
-
-  translate(-500, 100, -200);
-  for (var item = 0; item < leftPile.length; item++) {
-    translate(0, 0, -560);
+    // Draw Builidngs on left-hand side!
     push();
-    translate(0, -leftPile[item].height / 2, 0);
-    ambientMaterial(spectrum[0], spectrum[spectrum.length / 8], spectrum[spectrum.length / 4]);
-    leftPile[item].display(item);
+  
+    translate(-500, 100, -200);
+    for (var item = 0; item < leftPile.length; item++) {
+      translate(0, 0, -560);
+      push();
+      translate(0, -leftPile[item].height / 2, 0);
+      ambientMaterial(spectrum[0], spectrum[spectrum.length / 8], spectrum[spectrum.length / 4]);
+      leftPile[item].display(item);
+      pop();
+    }
+  
     pop();
-  }
-
-  pop();
-
-  // Draw Builidngs on righthand side!
-  push();
-
-  translate(500, 100, -300);
-  for (var item = 0; item < rightPile.length; item++) {
-    translate(0, 0, -600);
+  
+    // Draw Builidngs on righthand side!
     push();
-    translate(0, -rightPile[item].height / 2, 0);
-    ambientMaterial(spectrum[0], spectrum[spectrum.length / 8], spectrum[spectrum.length / 4]);
-    rightPile[item].display(item);
+  
+    translate(500, 100, -300);
+    for (var item = 0; item < rightPile.length; item++) {
+      translate(0, 0, -600);
+      push();
+      translate(0, -rightPile[item].height / 2, 0);
+      ambientMaterial(spectrum[0], spectrum[spectrum.length / 8], spectrum[spectrum.length / 4]);
+      rightPile[item].display(item);
+      pop();
+    }
+  
     pop();
-  }
+    
+    // draw white dots
+    push();
+    translate(180, 0, 50);
+    ambientMaterial(255);
+    for(var i=0; i<=600; i++) {
+      translate(0, 0, -180);
+      sphere(5);
+    }
+    pop();
+    
+    // draw white dots left side
+    push();
+    translate(-180, 0, 50);
+    //fill(191, 128, 255);
+    for(var i=0; i<=600; i++) {
+      translate(0, 0, -180);
+      sphere(5);
+    }
+    pop();
 
-  pop();
-  
-  // draw white dots
-  push();
-  translate(180, 0, 50);
-  ambientMaterial(255);
-  for(var i=0; i<=600; i++) {
-    translate(0, 0, -180);
-    sphere(5);
   }
-  pop();
-  
-  // draw white dots left side
-  push();
-  translate(-180, 0, 50);
-  //fill(191, 128, 255);
-  for(var i=0; i<=600; i++) {
-    translate(0, 0, -180);
-    sphere(5);
-  }
-  pop();
-
-
 }
 
 
